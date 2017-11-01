55a8bd55a26fd83e57b4016788e11780e72b81bb
Hardslog <hardslog@gmail.com>
2017-11-01 18:25:29 -0400

    add proptietary files from opr3.170623.008

---
+++ sailfish/BoardConfigVendor.mk
@@ -0,0 +1,2 @@
+# [2017-10-26] Auto-generated file, do not edit
+
---
+++ sailfish/BoardConfigVendorPartial.mk
@@ -0,0 +1,6 @@
+# [2017-10-26] Auto-generated file, do not edit
+
+TARGET_BOARD_INFO_FILE := vendor/google_devices/sailfish/vendor-board-info.txt
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_PARTITION_SIZE := 314572800
+BOARD_SEPOLICY_DIRS += device/google/marlin/sepolicy/verizon
